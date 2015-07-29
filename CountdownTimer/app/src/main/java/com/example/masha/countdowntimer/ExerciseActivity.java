bc59b48a8aefa41274c672ed11c4f7be061ced48
Bryant Young <youngbryant426@gmail.com>
2015-07-28 21:07:07 -0400

    Revert "Adding database content to main activity"
    This reverts commit 8b1c0c25bee53b107247629174a1253513ee08d7.

--- CountdownTimer/app/src/main/java/com/example/masha/countdowntimer/ExerciseActivity.java
+++ CountdownTimer/app/src/main/java/com/example/masha/countdowntimer/ExerciseActivity.java
@@ -8,41 +8,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.List;
-import java.util.Random;
 
 
 public class ExerciseActivity extends ActionBarActivity {
 
     private Button doneButton;
     private Button skipButton;
-    private CommentsDataSource datasource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
-        datasource = new CommentsDataSource(this);
-        datasource.open();
-
-        List<Comment> values = datasource.getAllComments();
-        String[] comments = new String[]{"Dumbbells Exercise: lift 3", "Intervals: run for 20 minutes", "Ab crunches" };
-        int nextInt = new Random().nextInt(3);
-        // globally
-        TextView myAwesomeTextView = (TextView)findViewById(R.id.timer_view);
-
-//in your OnCreate() method
-        Comment comment = null;
-        comment = datasource.createComment(comments[nextInt]);
-        //adapter.add(comment);
-
-        //adapter.notifyDataSetChanged();
-        //Toast toast = Toast.makeText(getApplicationContext(), R.string.exercise, duration);
-        String tester = comment.toString();
-        myAwesomeTextView.setText(tester);
 
         doneButton = (Button) findViewById(R.id.done_button);
 
@@ -68,7 +44,6 @@ public class ExerciseActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_exercise, menu);
-
         return true;
     }
 
