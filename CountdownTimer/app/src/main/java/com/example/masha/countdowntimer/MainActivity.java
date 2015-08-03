ed0a471f19682ccb76d31d49096b31dbd438ba14
amandasopkin <sopkin2@illinois.edu>
2015-08-02 19:08:28 -0700

    Got progress bar to restart every time!

--- CountdownTimer/app/src/main/java/com/example/masha/countdowntimer/MainActivity.java
+++ CountdownTimer/app/src/main/java/com/example/masha/countdowntimer/MainActivity.java
@@ -6,6 +6,8 @@ import android.app.TaskStackBuilder;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
@@ -42,8 +44,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int tracker = 25;
 
         mProgressBar = (ProgressBar)findViewById(R.id.adprogress_progressBar);
+
+        Resources res = getResources();
+        Drawable drawable = res.getDrawable(R.drawable.circular);
+        mProgressBar.setProgress(tracker);   // Main Progress
+        mProgressBar.setSecondaryProgress(50); // Secondary Progress
+        mProgressBar.setMax(100); // Maximum Progress
+        mProgressBar.setProgressDrawable(drawable);
         //showUserSettings();
 
         /**datasource = new CommentsDataSource(this);
@@ -128,6 +138,7 @@ public class MainActivity extends ActionBarActivity {
                 // mId allows you to update the notification later on.
                 mNotificationManager.notify(234234, mBuilder.build());
                 **/
+                mProgressBar.setProgress(0);
                 sendMessage();
             }
 
@@ -219,6 +230,35 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onResume() {
       super.onResume();
+        Resources res = getResources();
+        Drawable drawable = res.getDrawable(R.drawable.circular);
+        mProgressBar.setProgress(25);   // Main Progress
+        mProgressBar.setSecondaryProgress(50); // Secondary Progress
+        mProgressBar.setMax(100); // Maximum Progress
+        mProgressBar.setProgressDrawable(drawable);
+        //showUserSettings();
+
+        final Thread timerThread = new Thread() {
+            @Override
+            public void run() {
+                mbActive = true;
+                try {
+                    int waited = 0;
+                    while(mbActive && (waited < TIMER_RUNTIME)) {
+                        sleep(200);
+                        if(mbActive) {
+                            waited += 200;
+                            updateProgress(waited);
+                        }
+                    }
+                } catch(InterruptedException e) {
+                    // do nothing
+                } finally {
+                    onContinue();
+                }
+            }
+        };
+        timerThread.start();
         mCountDownTimer.create();
     }
 
