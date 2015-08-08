39f1fd48a42f37cec45551b6fef1a348a40e26f2
amandasopkin <sopkin2@illinois.edu>
2015-08-07 22:21:16 -0700

    Adding in detail fragments

---
+++ CountdownTimer/app/src/main/java/com/example/masha/countdowntimer/DetailActivity.java
@@ -0,0 +1,33 @@
+package com.example.masha.countdowntimer;
+
+/**
+ * Created by asopkin on 8/7/2015.
+ */
+import android.app.Activity;
+import android.app.Application;
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+public class DetailActivity extends Activity {
+    public static final String EXTRA_URL = "url";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // check if activity has been switched to landscape mode
+        // if yes, finish this activity
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            finish();
+            return;
+        }
+        setContentView(R.layout.activity_detail);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String url = extras.getString(EXTRA_URL);
+            DetailFragment detailFragment = (DetailFragment) getFragmentManager()
+                    .findFragmentById(R.id.detailFragment);
+            detailFragment.setText(url);
+        }
+    }
+}
